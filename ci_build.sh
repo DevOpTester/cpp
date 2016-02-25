5f8b0b1d776372144a2fb1b0e1e3ae24f67ddf88
Elijah <deleeke@gmail.com>
2016-02-25 15:23:23 -0500

    continue troubleshooting

--- ci_build.sh
+++ ci_build.sh
@@ -21,51 +21,15 @@ function install_trusty_dep()
 	if [ "$OSTYPE" = "linux-gnu" ];
 	then echo "Installing dependencies for ${OSTYPE}";
 	echo
-	#https://launchpad.net/~plfiorini/+archive/ubuntu/cmake
-#	echo "yes" | sudo add-apt-repository ppa:plfiorini/cmake ;
-	#https://wiki.ubuntu.com/ToolChain
-#	echo "yes" | sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 	time sudo apt-get update -qq
 	time sudo apt-get install cmake
 	time sudo apt-get install gcc-4.8 g++-4.8 -qq
     time sudo apt-get install gfortran -qq
     time sudo apt-get install valgrind
     rm valgrind.log*
-#	sudo apt-get upgrade -qq
 	echo "${CXX} will be your compiler"
 	CXX="g++";
 	fi
-}
-
-# following function install_MPI() adapted from https://github.com/mpi4py/mpi4py/blob/master/conf/travis/install-mpi.sh
-#Author: Lisandro Dalcin
-#Contact:	dalcinl@gmail.com
-#Copyright (c) 2015, Lisandro Dalcin. All rights reserved.
-
-function install_MPI()
-{
-    set -e
-    case $1 in
-        mpich1) set -x;
-            sudo apt-get install -q mpich-shmem-bin libmpich-shmem1.0-dev;;
-        mpich2) set -x;
-            sudo apt-get install -q mpich2 libmpich2-3 libmpich2-dev;;
-        mpich3) set -x;
-            sudo apt-get install -q gfortran libcr0 default-jdk;
-            wget http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz;
-    
-            mkdir $HOME/mpich3.2-install;
-            mv mpich-3.2.tar.gz $HOME/mpich3.2-install;
-            tar xfz $HOME/mpich3.2-install/mpich-3.2.tar.gz
-            cd $HOME/mpich3.2-install/
-            ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/mpich3.2-install --enable-fast make make install;;
-            cd $HOME
-        openmpi) set -x;
-            sudo apt-get install openmpi-bin openmpi-dev
-            export PATH=$HOME/mpich3.1-install/bin:$PATH;;
-        *)
-            echo "Unknown MPI implementation:" $1; exit 1;;
-    esac
 }
 
 function install_PETSc()
