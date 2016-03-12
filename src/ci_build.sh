2073c45e04f2ea0108bace589355ad49cada56c2
Elijah <deleeke@gmail.com>
2016-03-12 15:55:06 -0500

    attempt to build more deps on semaphore

--- src/ci_build.sh
+++ src/ci_build.sh
@@ -1,78 +1,198 @@
 #!/bin/sh
 # Installs PETSc, MPI, compilers, compiles and runs tests and main 
 
+function setup()
+{
+  mkdir $HOME/sfw/linux
+  mkdir $HOME/sfw/linux/fblaslapack/
+  export FBLASLAPACK_DIR=$HOME/sfw/linux/fblaslapack/
+  mkdir $HOME/sfw/linux/mpich/mpich2/
+  export MPICH_DIR=$HOME/sfw/linux/mpich/mpich2/
+  mkdir $HOME/sfw/linux/hypre/
+  export HYPRE_DIR=$HOME/sfw/linux/hypre/
+  mkdir $HOME/sfw/linux/boost/
+  export BOOST_DIR=$HOME/sfw/linux/boost/
+  mkdir $HOME/sfw/linux/hdf5/ 
+  export HDF5_DIR=$HOME/sfw/linux/hdf5/
+}
+
 function license_statement()
 {
     echo
     echo "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~"
-    echo "             testingCPP is a CI and C++ testing project"
     echo
     echo "                  Copyright (C) 2016 Elijah DeLee"
     echo "                  Licensed under the MIT License."
+    echo "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED"
     echo "         See the file LICENSE.txt for the full license text."
     echo
-    echo "         Written by Elijah DeLee <https://github.com/deleeke>"
     echo "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~"
     echo
 }
 
 function install_trusty_dep()
 {
-    echo "========================================="
+    CXX="g++";
+    C="gcc";
+    FC="gfortran";
     echo
-    echo "Installing dependencies for ${OSTYPE}";
+    echo "Installing dependencies for Ubuntu 14.04";
     echo
-    echo "=========================================="
-    time sudo apt-get update -y
+    sudo apt-get update -y
     echo "Installing valgrind"
     sudo apt-get install valgrind -y
-    rm valgrind.log*
-    echo "=========================================="
-    echo "=========================================="
-    time sudo apt-get install cmake
+    sudo apt-get install cmake -y
     sudo apt-get install libreadline6 -y
-    echo "=========================================="
-    time sudo apt-get install gcc -y
-    time sudo apt-get install g++ -y
-    echo "=========================================="
-    time sudo apt-get install gfortran -y
-    echo "=========================================="
-    echo "${CXX} will be your compiler"
-    CXX="g++";
+    sudo apt-get install gcc -y
+    sudo apt-get install g++ -y
+    sudo apt-get install gfortran -y
+    echo "$CXX will C++ be your compiler"
+    echo "$C will be your C compiler"
+    echo "$FC will be your fortran compiler"
+    
+}
+
+function install_SILO()
+{
+  # Download and install Silo 4.10
+  cd $HOME/sfw/linux/
+  mkdir silo-4.10
+  cd silo-4.10
+  export SILO_DIR=$HOME/sfw/linux/silo-4.10
+
+  wget https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10/silo-4.10.tar.gz
+
+  tar xvfz silo-4.10.tar.gz
+
+  ./configure \
+    CC=gcc \
+    CXX=g++ \
+    FC=gfortran \
+    F77=gfortran \
+    --prefix=$HOME/sfw/linux/silo/4.10 \
+    --disable-silex
+  make
+  make install
 }
 
 function install_PETSc()
 {
-  time git clone -b maint https://bitbucket.org/petsc/petsc $HOME/petsc
-  cd $HOME/petsc/
-  time ./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-fblaslapack --download-mpich --download-hypre --download-boost --download-hdf5
+  git clone -b maint https://bitbucket.org/petsc/petsc $HOME/sfw/petsc
+  cd $HOME/sfw/petsc/
+  export PETSC_DIR=$HOME/sfw/petsc/
+  export PETSC_ARCH=linux-dbg
+  ./configure \
+    --with-cc=gcc \
+    --with-cxx=g++ \
+    --with-fc=gfortran
+    --download-fblaslapack=$FBLASLAPACK_DIR \
+    --download-mpich=$MPICH_DIR \
+    --download-hypre=$HYPRE_DIR \
+    --with-shared-libraries \
+    --download-boost=$BOOST_DIR \
+    --download-hdf5=$HDF5_DIR \
+    --PETSC_ARCH=$PETSC_ARCH \
   time make all test 
+  export PETSC_DIR=$PWD
+  export PETSC_ARCH=linux-opt
+  ./configure \
+    --with-cc=gcc \
+    --with-cxx=g++ \
+    --with-fc=gfortran
+    --download-fblaslapack=$FBLASLAPACK_DIR \
+    --download-mpich=$MPICH_DIR \
+    --download-hypre=$HYPRE_DIR \
+    --with-shared-libraries \
+    --download-boost=$BOOST_DIR \
+    --download-hdf5=$HDF5_DIR \
+    --PETSC_ARCH=$PETSC_ARCH \
+    --with-debugging=0 \
+    --with-x=0 \
+  make
+  make test
+  
   cd $HOME 
 } 
 
-function install_trusty_dep()
+function install_SAMRAI()
 {
-    if [ "$OSTYPE" = "linux-gnu" ];
-    then echo "Installing dependencies for ${OSTYPE}";
-    echo
-    #https://launchpad.net/~plfiorini/+archive/ubuntu/cmake
-#   echo "yes" | sudo add-apt-repository ppa:plfiorini/cmake ;
-    #https://wiki.ubuntu.com/ToolChain
-#   echo "yes" | sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    time sudo apt-get update -qq
-    time sudo apt-get install cmake
-    time sudo apt-get install gcc-4.8 g++-4.8 -qq
-    time sudo apt-get install gfortran -qq
-#   sudo apt-get upgrade -qq
-    echo "${CXX} will be your compiler"
-    CXX="g++";
-    fi
+  cd $HOME/sfw
+  mkdir samrai
+  cd samrai
+  mkdir 2.4.4
+  cd 2.4.4
+  wget https://computation.llnl.gov/project/SAMRAI/download/SAMRAI-v2.4.4.tar.gz
+  tar xvfz SAMRAI-v2.4.4.tar.gz
+  wget https://github.com/IBAMR/IBAMR/releases/download/v0.1-rc1/SAMRAI-v2.4.4-patch-121212.gz
+  cd SAMRAI
+  ./source/scripts/includes --link
+  gunzip -c ../SAMRAI-v2.4.4-patch-121212.gz | patch -p2
+  
+  cd $HOME/sfw/samrai/2.4.4
+  mkdir objs-dbg
+  cd objs-dbg
+  ../SAMRAI/configure \
+    --prefix=$HOME/sfw/samrai/2.4.4/linux-dbg \
+    --with-CC=gcc \
+    --with-CXX=g++ \
+    --with-F77=gfortran \
+    --with-MPICC=$MPICH_DIR \
+    --with-hdf5=$HDF5_DIR \
+    --without-hypre \
+    --with-silo=$SILO_DIR \
+    --without-blaslapack \
+    --without-cubes \
+    --without-eleven \
+    --without-kinsol \
+    --without-petsc \
+    --without-sundials \
+    --without-x \
+    --with-doxygen \
+    --with-dot \
+    --enable-debug \
+    --disable-opt \
+    --enable-implicit-template-instantiation \
+    --disable-deprecated
+  make
+  make install
+  
+  cd $HOME/sfw/samrai/2.4.4
+  mkdir objs-opt
+  cd objs-opt
+  ../SAMRAI/configure \
+    CFLAGS="-O3" \
+    CXXFLAGS="-O3" \
+    FFLAGS="-O3" \
+    --prefix=$HOME/sfw/samrai/2.4.4/linux-opt \
+    --with-CC=gcc \
+    --with-CXX=g++ \
+    --with-F77=gfortran \
+    --with-MPICC=$MPICH_DIR \
+    --with-hdf5=$HDF5_DIR \
+    --without-hypre \
+    --with-silo=$SILO_DIR \
+    --without-blaslapack \
+    --without-cubes \
+    --without-eleven \
+    --without-kinsol \
+    --without-petsc \
+    --without-sundials \
+    --without-x \
+    --with-doxygen \
+    --with-dot \
+    --disable-debug \
+    --enable-opt \
+    --enable-implicit-template-instantiation \
+    --disable-deprecated
+  make
+  make install 
 }
 
 #Call functions
-
+setup
 license_statement
-#install_MPI
 install_trusty_dep
+install_SILO
 install_PETSc
+install_SAMRAI
 exit 0
