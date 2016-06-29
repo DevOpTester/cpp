9506650eac7513d3430df282643ca0ea1ca33c3e
Elijah <deleeke@gmail.com>
2016-06-29 14:12:27 -0400

    Convert to cmake

--- src/CMakeFiles/runTests.dir/cmake_clean.cmake
+++ src/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/runTests.dir/tests.cpp.o"
   "runTests.pdb"
   "runTests"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
